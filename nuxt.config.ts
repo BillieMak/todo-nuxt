dcd24278ed97911aabf4c5f31cf44ed5fab26372
David Rivera <nicriver_40@hotmail.com>
2024-03-04 11:43:10 -0500

    preparing module primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    'nuxt-primevue'
+  ],
+  primevue :{
+    options:{
+      ripple: true
+    },
+    components: {
+      include:[
+        'Button',
+      ]
+    },
+   
+  },
+  css: ['primevue/resources/themes/aura-light-green/theme.css'],
 })
